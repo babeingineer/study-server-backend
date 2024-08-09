d094010800974ceb978f7f18b2c389edde096982
babeingineer <babeengineer@proton.me>
2024-08-09 09:33:49 -0500

    hahaha

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response } from "express"
 import axios from "axios"
-import { sendMail } from "./mailer"
 import path from "path"
 import cors from "cors"
+import moment from "moment"
 
 const rdpInfo = {
   "server": "34.224.109.221",
@@ -15,26 +15,25 @@ const app = express();
 const ec2BackendUrl = process.env.EC2_BACKEND_URL;
 const backendIp = process.env.BACKEND_IP;
 
-const staticDir = path.join(__dirname, "../static");
 app.use(cors());
-app.use(express.static(staticDir));
 
 app.get("/launch", async (req: Request, res: Response) => {
 
-  axios.get(`${ec2BackendUrl}/launch`, {params: {id: req.query.id}});
+  axios.get(`${ec2BackendUrl}/launch`, {params: {id: moment().format('YYYY-MM-DD-HH-mm-ss')}});
   const passwordHash = (await axios.get(`http://${backendIp}/Myrtille/GetHash.aspx`, { params: { password: rdpInfo.password } })).data;
   res.redirect(encodeURI(`http://${backendIp}/Myrtille/?__EVENTTARGET=&__EVENTARGUMENT=&server=${rdpInfo.server}&user=${rdpInfo.user}&passwordHash=${passwordHash}&connect=Connect`))
 });
 
 
-app.get("/invite", async (req: Request, res: Response) => {
-  const { email } = req.query;
-  sendMail(email as string, "Invitation to study", `Hi, please participate in the study here http://${backendIp}/launch?id=ondriah`);
-
-});
 
 app.get("/logs", async (req: Request, res: Response) => {
   res.send((await axios.get(`${ec2BackendUrl}/logs`, {params: {id: req.query.id}})).data);
+});
+
+app.get("/ids", async (req: Request, res: Response) => {
+  const { data } = await axios.get(`${ec2BackendUrl}/ids`);
+
+  res.send(data);
 })
 
 app.listen(8001, () => {
